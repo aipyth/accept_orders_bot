e83393792124f53bc16ddba80c33166b95deb64b
Ivan <aipyth.func@gmail.com>
2021-06-20 00:31:24 +0300

    db initialization added

--- bot/db/config.js
+++ bot/db/config.js
@@ -8,11 +8,20 @@ require('dotenv').config()
 //   password: process.env.POSTGRES_PASSWORD,
 //   port: process.env.POSTGRES_PORT
 // }))
-
-module.exports = new Pool({
+const pool = new Pool({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT
 })
+pool.query(`create table if not exists BotUsers (
+    id          bigint          primary key,
+    username    varchar(255)    not null,
+    "name"      varchar(255)    not null,
+    surname     varchar(255)    not null,
+
+    created_at  timestamp       default now()
+);`)
+
+module.exports = pool
